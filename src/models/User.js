469e61e0a3127f8766c981829400e546a8f32217
SurajM <surajmargale777@gmail.com>
2025-09-24 16:16:16 +0530

    seed data

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  avatarUrl: { type: String, default: "" },
-  bio: { type: String, default: "" },
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  followersCount: { type: Number, default: 0 },
-  followingCount: { type: Number, default: 0 },
-}, { timestamps: true });
+    username: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    passwordHash: {type: String, required: true},
+    avatarUrl: {type: String, default: ""},
+    bio: {type: String, default: ""},
+    followers: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    following: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    followersCount: {type: Number, default: 0},
+    followingCount: {type: Number, default: 0},
+    likedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "Recipe"}]
+}, {timestamps: true});
 
 
 module.exports = mongoose.model("User", userSchema);
